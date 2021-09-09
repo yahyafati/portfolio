36371b664628faefefa167192aea17f13d0ede9c
Yahya Fati <yfati037@gmail.com>
2021-09-09 17:43:10 +0300

    added react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloWorld from "./component/Hellworld";
+
+const App = () => {
+    return (
+        <div id="App">
+            <HelloWorld />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/component/Hellworld.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return <h3>So is this really working</h3>;
+};
+
+export default HelloWorld;
