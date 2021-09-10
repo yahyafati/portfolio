52cff180155089a348226bf15fef3be5844cc7f7
Yahya Fati <yfati037@gmail.com>
2021-09-10 18:15:34 +0300

    minor 640 responsiveness changes

--- src/component/testimonial/TestimonialItem.js
+++ src/component/testimonial/TestimonialItem.js
@@ -11,8 +11,8 @@ const TestimonialItem = () => {
             />
             <p className="body">
                 Yahya is the best developer ever, he can beat anyone in a fight.
-                I’m not just saying that because I’m his mother. He can beat
-                your....
+                I’m not just saying that because I’m his mother. My son is
+                better than yours.
             </p>
             <div className="credit">
                 <p className="by">Nuria Mussa</p>
