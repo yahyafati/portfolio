7bd9587e52220e0224a31bad4f945e258e0195f3
Yahya Fati <yfati037@gmail.com>
2021-09-10 09:11:34 +0300

    added home page intro

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "../../style/home/home.css";
+import standingManSVG from "../../svg/Home.svg";
 
 const Home = () => {
-    return <div></div>;
+    return (
+        <div id="homeSection">
+            <div className="intro_text">
+                <p className="hi">Hi, I'm</p>
+                <p className="name">Yahya Fati</p>
+                <p className="work">Freelance Web and Mobile Developer</p>
+                <div className="buttons">
+                    <a className="button primary">Contact Me</a>
+                    <a className="button">Download CV</a>
+                </div>
+            </div>
+            <img
+                className="introImg"
+                src={standingManSVG}
+                alt="A Man Standing and Using Mobile"
+            />
+        </div>
+    );
 };
 
 export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,17 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "images/[hash]-[name].[ext]",
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
