5cd42ea1e963d9b511c37db4e4aa798be7049609
Yahya Fati <yfati037@gmail.com>
2021-09-14 13:46:45 +0300

    added social media links

--- src/component/landingPage/contact/SocialMediaIcons.js
+++ src/component/landingPage/contact/SocialMediaIcons.js
@@ -2,16 +2,29 @@ import React from "react";
 import {FaFacebookF, FaLinkedinIn, FaPhone, FaTwitter} from "react-icons/fa";
 import {MdEmail} from "react-icons/md";
 import {AiFillInstagram} from "react-icons/ai";
+import {Link} from "react-router-dom";
 
 const SocialMediaIcons = () => {
     return (
         <div className="icons">
-            <FaTwitter className="icon"/>
-            <FaLinkedinIn className="icon"/>
-            <FaFacebookF className="icon"/>
-            <MdEmail className="icon"/>
-            <AiFillInstagram className="icon"/>
-            <FaPhone className="icon"/>
+            <a href={"https://www.twitter.com"}>
+                <FaTwitter className="icon"/>
+            </a>
+            <a href={"https://www.linkedin.com/in/yahya-haji"}>
+                <FaLinkedinIn className="icon"/>
+            </a>
+            <a href={"https://www.facebook.com/yfati87"}>
+                <FaFacebookF className="icon"/>
+            </a>
+            <a href={"https://www.instagram.com/yahyafati8"}>
+                <AiFillInstagram className="icon"/>
+            </a>
+            <a href={"mailto:yfati37@gmail.com"}>
+                <MdEmail className="icon"/>
+            </a>
+            <a href={"tel:+905394909673"}>
+                <FaPhone className="icon"/>
+            </a>
         </div>
     );
 };
--- src/component/landingPage/shallWe/ShallWe.js
+++ src/component/landingPage/shallWe/ShallWe.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../../../style/contact/contact.css";
+import {Link} from "react-router-dom";
 
 const ShallWe = () => {
     return (
         <div id="shallWeSection">
             <hr className="h-line"/>
-            <div className="text-container">
+            <Link to={"/contact"} className="text-container">
                 <h4 className="shallWe">Shall We Start?</h4>
-                <a className="letsGo">Let's Go</a>
-            </div>
+                <p className="letsGo">Let's Go</p>
+            </Link>
             <hr className="h-line"/>
         </div>
     );
