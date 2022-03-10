18bd3f1091042c736187886fc2139d8382f023de
Yahya Haji <yfati037@gmail.com>
2022-03-10 12:24:03 +0300

    added typing animation

--- src/component/landingPage/home/Home.js
+++ src/component/landingPage/home/Home.js
@@ -1,16 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../../style/home/home.css";
 import standingManSVG from "../../../svg/Home.svg";
 import downloadableFile from "../../../Resume.docx";
 import { Link } from "react-router-dom";
 
+const MY_TITLE = ["Web Developer", "Mobile Developer", "Student"];
 const Home = () => {
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [animateTyping, setAnimateTyping] = useState(true);
+
+    useEffect(() => {
+        setAnimateTyping(true);
+    }, [currentWordIndex]);
+
+    const handleAnimationEnd = (e) => {
+        if (e.animationName === "slide") {
+            setAnimateTyping(false);
+            console.log(e.target.classList);
+            setTimeout(() => {
+                setCurrentWordIndex(
+                    (current) => (current + 1) % MY_TITLE.length
+                );
+            }, 3000);
+        }
+    };
+
     return (
         <div id="homeSection">
             <div className="intro_text">
                 <p className="hi">Hi, I'm</p>
                 <p className="name">Yahya Fati</p>
-                <p className="work">Freelance Web and Mobile Developer</p>
+                <p className="work" onAnimationEnd={handleAnimationEnd}>
+                    <span
+                        className={`before ${animateTyping ? "animate" : ""}`}
+                        style={{
+                            animationTimingFunction: `steps(${MY_TITLE[currentWordIndex].length})`,
+                        }}
+                    >
+                        <span
+                            className={`after ${
+                                animateTyping ? "animate" : ""
+                            }`}
+                            style={{
+                                animationTimingFunction: `steps(${MY_TITLE[currentWordIndex].length})`,
+                            }}
+                        ></span>
+                    </span>
+
+                    {MY_TITLE[currentWordIndex]}
+                </p>
                 <div className="buttons">
                     <Link to={"/contact"} className="button primary">
                         Contact Me
@@ -28,7 +66,7 @@ const Home = () => {
             <img
                 className="introImg"
                 src={standingManSVG}
-                alt="A Man Standing and Using Mobile"
+                alt="A Man Standing and Using a phone"
             />
         </div>
     );
